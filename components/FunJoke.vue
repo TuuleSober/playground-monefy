580913b3668aa3e6f4d9f969eec3a661b8808e9b
Tuule Sõber <tuule@infogrid.io>
2022-01-07 14:57:38 +0200

    Initial project

---
+++ components/FunJoke.vue
@@ -0,0 +1,25 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div
+    class="relative flex items-top justify-center flex-col min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+  >
+    <a class="mb-4">When do you want to hear racecar noises?</a>
+    <button v-on:click="say">
+    Click to reveal {{aa}}
+  </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SoberIndustries',
+  methods: {
+    say: () => {
+      alert('neooowwww')
+    }
+  },
+  data: () => ({
+    aa: "joke",
+  })
+}
+</script>
